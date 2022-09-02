f7f8436f92293aabdfe8b15740a924d5490644d2
Jongco <narira0922@gmail.com>
2022-09-02 20:09:41 +0900

    refactor: 코드 컨벤션에 맞게 리팩토링

--- src/components/SetTimeModal.jsx
+++ src/components/SetTimeModal.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import styles from'../css/setTimeModal.module.css';
 
-const SetTimeModal = ({target, setTarget, setMode, second, minute, hour, setRunning}) => {
+const SetTimeModal = ({setTarget, setMode}) => {
 
     const targetHour = useRef();
     const targetMinute = useRef();
@@ -13,8 +13,8 @@ const SetTimeModal = ({target, setTarget, setMode, second, minute, hour, setRunn
             <input type="number" ref={targetMinute} min='0' max='59' defaultValue='0'/>
             <input type="number" ref={targetSecond} min='0' max='59' defaultValue='0'/>
             <button onClick={() => {
-                setTarget({hour: Number(targetHour.current.value), minute: Number(targetMinute.current.value), second: Number(targetSecond.current.value)})
-                setMode('normal')
+                setTarget({hour: Number(targetHour.current.value), minute: Number(targetMinute.current.value), second: Number(targetSecond.current.value)});
+                setMode('normal');
             }}>시간 설정</button>
         </div>
     );
--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import SetTimeModal from './SetTimeModal';
 
 const Stopwatch = () => {
-    const [target, setTarget] = useState({})
-    const [second, setSecond] = useState(0);
-    const [minute, setMinute] = useState(0);
+
+    const [target, setTarget] = useState({});
     const [hour, setHour] = useState(0);
-    
+    const [minute, setMinute] = useState(0);
+    const [second, setSecond] = useState(0);
     const [mode, setMode] = useState('normal');
     const [running, setRunning] = useState(false);
     
@@ -16,30 +15,30 @@ const Stopwatch = () => {
         let interval;
         if(running){
             interval = setInterval(() => {
-                setSecond(prev => prev + 1)
+                setSecond(prev => prev + 1);
             }, 1000);
         } else {
-            clearInterval(interval)
+            clearInterval(interval);
         }
         return () => clearInterval(interval);
     },[running]);
 
     useEffect(() => {
         if(second === 60){
-            setSecond(0)
-            setMinute(prev => prev + 1)
+            setSecond(0);
+            setMinute(prev => prev + 1);
         } else if (minute === 60) {
-            setMinute(0)
-            setHour(prev => prev + 1)
+            setMinute(0);
+            setHour(prev => prev + 1);
         }
     },[hour, minute, second]);
 
     useEffect(() => {
         if(target !== {} && target.hour === hour && target.minute === minute && target.second === second){
-            alert('끝')
-            setRunning(false)
+            alert('끝');
+            setRunning(false);
         }
-    }, [target, hour, minute, second, setRunning])
+    }, [target, hour, minute, second, setRunning]);
 
 
     return (
@@ -48,18 +47,18 @@ const Stopwatch = () => {
             {
                 mode === 'normal' ? 
                 <button onClick={() => {
-                    setMode('set')
+                    setMode('set');
                 }}>시간 설정</button>
                 :
                 <button onClick={() => {
-                    setMode('normal')
+                    setMode('normal');
                 }}>닫기</button>
             }
             <button onClick={() => {
-                setRunning(true)
+                setRunning(true);
             }}>스톱워치 시작</button>
             <button onClick={() => {
-                setRunning(false)
+                setRunning(false);
             }}>스톱워치 멈춰</button>
             {mode === 'set' && <SetTimeModal setMode={setMode} setTarget={setTarget}/>}
         </div>
