e71bb4af976596b0a07e97aae2f401dfc7210db5
Jongco <narira0922@gmail.com>
2022-09-02 19:47:25 +0900

    fix: alert창 여러 번 등장 및 warning message 해결

--- src/components/SetTimeModal.jsx
+++ src/components/SetTimeModal.jsx
@@ -1,26 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from'../css/setTimeModal.module.css';
 
-const SetTimeModal = ({setMode, second, minute, hour, setRunning}) => {
+const SetTimeModal = ({mode, setMode, second, minute, hour, setRunning}) => {
 
     const [target, setTarget] = useState({})
     const targetHour = useRef();
     const targetMinute = useRef();
     const targetSecond = useRef();
 
-    if(target !== {} && target.hour === hour && target.minute === minute && target.second === second){
-        setRunning(false)
-        alert('끝')
-    }
+    useEffect(() => {
+        if(target !== {} && target.hour === hour && target.minute === minute && target.second === second){
+            alert('끝')
+            setRunning(false)
+        }
+    }, [target, hour, minute, second, setRunning])
 
     return (
         <div className={styles.modal}>
-            <input type="number" ref={targetHour} min='0' max='23'/>
-            <input type="number" ref={targetMinute} min='0' max='59'/>
-            <input type="number" ref={targetSecond} min='0' max='59'/>
+            <input type="number" ref={targetHour} min='0' max='23' defaultValue='0'/>
+            <input type="number" ref={targetMinute} min='0' max='59' defaultValue='0'/>
+            <input type="number" ref={targetSecond} min='0' max='59' defaultValue='0'/>
             <button onClick={() => {
-                setTarget({...target, hour: Number(targetHour.current.value), minute: Number(targetMinute.current.value), second: Number(targetSecond.current.value)})
-                // setMode('normal')
+                setTarget({hour: Number(targetHour.current.value), minute: Number(targetMinute.current.value), second: Number(targetSecond.current.value)})
             }}>시간 설정</button>
         </div>
     );
