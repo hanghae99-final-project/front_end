35b3cd16572b2faefddd333a1502a334b26bfe2c
Jongco <narira0922@gmail.com>
2022-09-07 22:13:29 +0900

    docs: 수환님과 중간 병합 성공

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mainSlice from './slice/mainSlice';
-import rankingSlice from './slice/rankingSlice';
 import toDoSlice from './slice/toDoSlice';
+import rankingSlice from './slice/rankingSlice';
 
 export const store = configureStore({
     reducer: {
         main: mainSlice.reducer,
-        ranking: rankingSlice.reducer,
         toDo: toDoSlice.reducer,
+        ranking: rankingSlice.reducer,
     },
 });
