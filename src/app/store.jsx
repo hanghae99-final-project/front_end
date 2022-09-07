53550f5b4b9f2ab62124850edaf963fae58a4381
Jongco <narira0922@gmail.com>
2022-09-07 22:08:44 +0900

    docs: timePicker key값 오류 수정

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mainSlice from './slice/mainSlice';
+import rankingSlice from './slice/rankingSlice';
 
 export const store = configureStore({
     reducer: {
         main: mainSlice.reducer,
+        ranking: rankingSlice.reducer,
     },
 });
