7d76ff1b533760760ed8f34fb9f42fd2316702a2
Jongco <narira0922@gmail.com>
2022-09-08 11:28:12 +0900

    feature: add mypage weekly graph initial commit

---
+++ src/components/WeeklyDataGraph.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeeklyDataGraph = () => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+};
+
+export default WeeklyDataGraph;
---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import WeeklyDataGraph from '../components/WeeklyDataGraph';
+const MyPage = () => {
+    return (
+        <div>
+            <WeeklyDataGraph />
+        </div>
+    );
+};
+
+export default MyPage;
