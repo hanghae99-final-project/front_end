fd63244114485ae17d5f7f2265969091d866f4f4
H4nHS <gksgytmd19@kakao.com>
2022-09-14 15:24:14 +0900

    fix : 프로필수정 추가

---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/common/Layout";
+import ProfileModify from "../components/profile/ProfileModify";
+
+const MyPage = () => {
+  return (
+    <Layout>
+      <ProfileModify />
+    </Layout>
+  );
+};
+
+export default MyPage;
