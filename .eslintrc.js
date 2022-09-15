563cf8043b9c21c0fd6524d0e2756740672fea67
Jongco <narira0922@gmail.com>
2022-09-15 15:44:32 +0900

    fix: line break 해결 시도

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    rules: {
+        'linebreak-style': 0,
+    },
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false, // <== ADD THIS
+};
--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import Nickname from "../components/join/nickname";
-import Layout from "../components/common/Layout";
+import React, { useState } from 'react';
+import Nickname from '../components/join/nickname';
+import Layout from '../components/common/Layout';
 const Join = () => {
-  const [mode, setMode] = useState("Access");
+    const [mode, setMode] = useState('Access');
 
-  return (
-    <Layout>
-      <Nickname />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Nickname />
+        </Layout>
+    );
 };
 
 export default Join;
