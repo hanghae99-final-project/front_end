7678fe3c940dba9265a230b1bb3042decc079aff
Jongco <narira0922@gmail.com>
2022-09-16 16:09:42 +0900

    fix: mypage 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import WeeklyDataGraph from '../components/WeeklyDataGraph';
 import Layout from '../components/common/Layout';
 import Footer from '../components/common/Footer';
-import ProfileModify from '../components/profile/ProfileModify';
 import MyPageStudyTime from '../components/MyPageStudyTime';
 import { ReactComponent as Person } from '../svg/person.svg';
 import { ReactComponent as Event } from '../svg/event_available.svg';
 import styles from '../css/mypage.module.css';
 import ProfileTodoList from '../components/profile/ProfileTodoList';
+import { Link } from 'react-router-dom';
 
 const MyPage = () => {
     return (
         <Layout>
             <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-                {/* <ProfileModify /> */}
                 <MyPageStudyTime />
                 <div style={{ display: 'flex', gap: '10px' }}>
-                    <button className={styles.profileButton}>
-                        <Person />
-                        <span>프로필 설정</span>
-                    </button>
+                    <Link to='/modify'>
+                        <button className={styles.profileButton}>
+                            <Person />
+                            <span>프로필 설정</span>
+                        </button>
+                    </Link>
                     <button className={styles.profileButton}>
                         <Event />
                         <span>디데이 설정</span>
