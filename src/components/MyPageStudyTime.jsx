de038cc3990b334af83190b6af69fc52fe229d00
Jongco <narira0922@gmail.com>
2022-09-16 15:37:17 +0900

    fix: mypage 기능 배치 및 수정 완료

--- src/components/MyPageStudyTime.jsx
+++ src/components/MyPageStudyTime.jsx
@@ -1,13 +1,70 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import changeTimeSimpleForm from './changeTimeSimpleForm';
+import changeTimeForm from './changeTimeForm';
+import styles from '../css/mypage.module.css';
+import axios from 'axios';
+import { get_studytime } from '../app/slice/timeTimerSlice';
 
 const MyPageStudyTime = () => {
+    const date = new Date().getTime();
     const dispatch = useDispatch();
+    const [totalStudyTime, setTotalStudyTime] = useState(0);
+    const [run, setRun] = useState(false); // 타임타이머 동작 여부
+    const [second, setSecond] = useState(0); // just '초'
     const studyStartPoint = useSelector((state) => state.timer?.studyStartPoint);
     const savedStudyTime = useSelector((state) => state.timer?.savedStudyTime);
-    const restStartPoint = useSelector((state) => state.timer?.restStartPoint);
-    const savedRestTime = useSelector((state) => state.timer?.savedRestTime);
-    return <div></div>;
+
+    useEffect(() => {
+        __getTotalStudyTime();
+    }, []);
+
+    useEffect(() => {
+        dispatch(get_studytime());
+    }, [dispatch]);
+
+    const __getTotalStudyTime = async () => {
+        const { data } = await axios.get(process.env.REACT_APP_SERVER_URL + `/mypage/getTotalStudyTime`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.token}`,
+            },
+        });
+        console.log(data);
+        setTotalStudyTime(Math.floor(data.totalStudyTime / 1000));
+    };
+
+    useEffect(() => {
+        let interval;
+        if (run) {
+            interval = setInterval(() => {
+                setSecond((prev) => prev + 1);
+            }, 1000);
+        } else if (!run) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [run]);
+
+    useEffect(() => {
+        if (studyStartPoint === 0) {
+            setSecond(Math.floor(savedStudyTime / 1000));
+        } else {
+            setSecond(Math.floor((savedStudyTime + date - studyStartPoint) / 1000));
+            setRun(true);
+        }
+    }, [savedStudyTime, studyStartPoint]);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.aboveArea}>
+                <div className={styles.title}>오늘 공부시간</div>
+                {changeTimeForm(second, styles.todayStudyTime)}
+                <div className={styles.text}>누적 공부시간은 {changeTimeSimpleForm(totalStudyTime, styles.totalTime)}이에요</div>
+            </div>
+        </div>
+    );
 };
 
 export default MyPageStudyTime;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -4,13 +4,31 @@ import Layout from '../components/common/Layout';
 import Footer from '../components/common/Footer';
 import ProfileModify from '../components/profile/ProfileModify';
 import MyPageStudyTime from '../components/MyPageStudyTime';
+import { ReactComponent as Person } from '../svg/person.svg';
+import { ReactComponent as Event } from '../svg/event_available.svg';
+import styles from '../css/mypage.module.css';
+import ProfileTodoList from '../components/profile/ProfileTodoList';
 
 const MyPage = () => {
     return (
         <Layout>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                 {/* <ProfileModify /> */}
                 <MyPageStudyTime />
+                <div style={{ display: 'flex', gap: '10px' }}>
+                    <button className={styles.profileButton}>
+                        <Person />
+                        <span>프로필 설정</span>
+                    </button>
+                    <button className={styles.profileButton}>
+                        <Event />
+                        <span>디데이 설정</span>
+                    </button>
+                </div>
+                <div style={{ display: 'flex', padding: '0.7rem', gap: '10px' }}>
+                    <ProfileTodoList />
+                    <ProfileTodoList />
+                </div>
                 <WeeklyDataGraph />
                 <Footer />
             </div>
