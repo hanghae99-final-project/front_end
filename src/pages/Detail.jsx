b99276fb0b842c1dc924e52184fa3c71af2964fb
Jongco <narira0922@gmail.com>
2022-09-16 11:00:57 +0900

    feature: login page 완료

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ReactComponent as KakaoLoginButton } from '../svg/kakao_icon.svg';
+import { ReactComponent as LoginSplash } from '../svg/loginSplash_background.svg';
+import { ReactComponent as Logo } from '../svg/logo.svg';
 import Layout from '../components/common/Layout';
+import styles from '../css/loginPage.module.css';
 
 const Detail = () => {
     const authURI = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
 
     return (
         <Layout>
+            <LoginSplash className={styles.background} />
+            <Logo className={styles.logo} />
             <KakaoLoginButton
+                className={styles.kakaoButton}
                 onClick={() => {
                     window.location.href = authURI;
                 }}
