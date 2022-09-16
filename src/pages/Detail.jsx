a06f597cc2488c26e1698aa064ff14fdb737fc4b
Jongco <narira0922@gmail.com>
2022-09-16 10:16:54 +0900

    feature: 로그인 페이지 생성

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
-import { get_login } from '../app/slice/mainSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { ReactComponent as KakaoLoginButton } from '../svg/kakao_icon.svg';
+import Layout from '../components/common/Layout';
 
 const Detail = () => {
     const authURI = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
 
     return (
-        <div>
-            <button
+        <Layout>
+            <KakaoLoginButton
                 onClick={() => {
                     window.location.href = authURI;
-                }}>
-                카카오 로그인
-            </button>
-        </div>
+                }}
+            />
+        </Layout>
     );
 };
 
