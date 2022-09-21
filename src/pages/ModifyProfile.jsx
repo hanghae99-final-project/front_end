86d041e246d262061db6aa2c0e9cf513566af30b
Jongco <narira0922@gmail.com>
2022-09-22 08:07:24 +0900

    design: ranking page 반응형 디자인 추가

--- src/pages/ModifyProfile.jsx
+++ src/pages/ModifyProfile.jsx
@@ -9,153 +9,144 @@ import Layout from "../layout/Layout";
 import styles from "./css/profile.module.css";
 
 const ModifyProfile = () => {
-  const pickAge = ["20대", "30대", "기타"];
-  const major = [
-    "경영사무",
-    "마케팅·광고·홍보",
-    "디자인",
-    "의료",
-    "영업·고객상담",
-    "IT개발·인터넷",
-    "전문·특수·연구직",
-    "미디어·문화",
-    "교육",
-    "서비스",
-    "연구개발·설계",
-    "관광레저서비스",
-    "건설·건축",
-    "공무원",
-    "무역·유통",
-  ];
-  const dispatch = useDispatch();
+    const pickAge = ["20대", "30대", "기타"];
+    const major = [
+        "경영사무",
+        "마케팅·광고·홍보",
+        "디자인",
+        "의료",
+        "영업·고객상담",
+        "IT개발·인터넷",
+        "전문·특수·연구직",
+        "미디어·문화",
+        "교육",
+        "서비스",
+        "연구개발·설계",
+        "관광레저서비스",
+        "건설·건축",
+        "공무원",
+        "무역·유통",
+    ];
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(__getProfile());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(__getProfile());
+    }, [dispatch]);
 
-  const userData = useSelector((data) => data.profile);
-  const initialState = {};
+    const userData = useSelector((data) => data.profile);
+    const initialState = {};
 
-  useEffect(() => {
-    if (userData.nickname !== undefined) {
-      setModifyInfo({
-        nickname: userData.nickname,
-        ageGroup: userData.ageGroup,
-        specialty: userData.specialty,
-      });
-    }
-  }, [userData]);
+    useEffect(() => {
+        if (userData.nickname !== undefined) {
+            setModifyInfo({
+                nickname: userData.nickname,
+                ageGroup: userData.ageGroup,
+                specialty: userData.specialty,
+            });
+        }
+    }, [userData]);
 
-  const [userNickname, setUserNickname] = useState(userData.nickname);
-  const [userAge, setUserAge] = useState();
-  const [userSpecialty, setUserSpecialty] = useState();
+    const [userNickname, setUserNickname] = useState(userData.nickname);
+    const [userAge, setUserAge] = useState();
+    const [userSpecialty, setUserSpecialty] = useState();
 
-  const [modifyInfo, setModifyInfo] = useState(initialState);
+    const [modifyInfo, setModifyInfo] = useState(initialState);
 
-  const onChangeHandleInput = (e) => {
-    const { name, value } = e.target;
-    setModifyInfo({ ...modifyInfo, [name]: value });
-  };
+    const onChangeHandleInput = (e) => {
+        const { name, value } = e.target;
+        setModifyInfo({ ...modifyInfo, [name]: value });
+    };
 
-  const onUpdate = () => {
-    dispatch(__updateProfile(modifyInfo));
-  };
+    const onUpdate = () => {
+        dispatch(__updateProfile(modifyInfo));
+    };
 
-  return (
-    <Layout>
-      <div className={styles.container}>
-        <div className={styles.topBox}>
-          <ArrowBack />
-          <span>프로필 수정</span>
-          <button onClick={onUpdate}>저장</button>
-        </div>
-        <div className={styles.emailBox}>
-          <p>로그인 계정</p>
-          <input
-            type="text"
-            placeholder="test@test.com"
-            disabled={true}
-          ></input>
-        </div>
-        <div className={styles.nicknameBox}>
-          <p>닉네임</p>
-          <input
-            type="text"
-            name="nickname"
-            value={modifyInfo.nickname}
-            onChange={onChangeHandleInput}
-            placeholder={userData.nickname}
-            autoComplete="off"
-            autoFocus={true}
-            maxLength="8"
-          ></input>
-          <p className={styles.checkMsg}>특수문자는 사용 불가능해요</p>
-        </div>
-        <div className={styles.ageBox}>
-          <p>연령대</p>
-          <div className={styles.ageContainer}>
-            {pickAge.map((pick, i) => {
-              return (
-                <div key={i}>
-                  {modifyInfo.ageGroup === pick ? (
-                    <button
-                      className={styles.agePick}
-                      onClick={() => {
-                        setModifyInfo({ ...modifyInfo, ageGroup: pick });
-                      }}
-                    >
-                      <Check />
-                      {pick}
-                    </button>
-                  ) : (
-                    <button
-                      className={styles.ageAnother}
-                      onClick={() => {
-                        setModifyInfo({ ...modifyInfo, ageGroup: pick });
-                      }}
-                    >
-                      {pick}
-                    </button>
-                  )}
+    return (
+        <Layout>
+            <div className={styles.container}>
+                <div className={styles.topBox}>
+                    <ArrowBack />
+                    <span>프로필 수정</span>
+                    <button onClick={onUpdate}>저장</button>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className={styles.specBox}>
-          <p>준비하고 있는 분야</p>
-          <div className={styles.specContainer}>
-            {major.map((pick, i) => {
-              return (
-                <div key={i}>
-                  {modifyInfo.specialty === pick ? (
-                    <button
-                      className={styles.specPick}
-                      onClick={() => {
-                        setModifyInfo({ ...modifyInfo, specialty: pick });
-                      }}
-                    >
-                      <Check />
-                      {pick}
-                    </button>
-                  ) : (
-                    <button
-                      className={styles.specAnother}
-                      onClick={() => {
-                        setModifyInfo({ ...modifyInfo, specialty: pick });
-                      }}
-                    >
-                      {pick}
-                    </button>
-                  )}
+                <div className={styles.emailBox}>
+                    <p>로그인 계정</p>
+                    <input type='text' placeholder='test@test.com' disabled={true}></input>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+                <div className={styles.nicknameBox}>
+                    <p>닉네임</p>
+                    <input
+                        type='text'
+                        name='nickname'
+                        value={modifyInfo.nickname}
+                        onChange={onChangeHandleInput}
+                        placeholder={userData.nickname}
+                        autoComplete='off'
+                        autoFocus={true}
+                        maxLength='8'></input>
+                    <p className={styles.checkMsg}>특수문자는 사용 불가능해요</p>
+                </div>
+                <div className={styles.ageBox}>
+                    <p>연령대</p>
+                    <div className={styles.ageContainer}>
+                        {pickAge.map((pick, i) => {
+                            return (
+                                <div key={i}>
+                                    {modifyInfo.ageGroup === pick ? (
+                                        <button
+                                            className={styles.agePick}
+                                            onClick={() => {
+                                                setModifyInfo({ ...modifyInfo, ageGroup: pick });
+                                            }}>
+                                            <Check />
+                                            {pick}
+                                        </button>
+                                    ) : (
+                                        <button
+                                            className={styles.ageAnother}
+                                            onClick={() => {
+                                                setModifyInfo({ ...modifyInfo, ageGroup: pick });
+                                            }}>
+                                            {pick}
+                                        </button>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+
+                <div className={styles.specBox}>
+                    <p>준비하고 있는 분야</p>
+                    <div className={styles.specContainer}>
+                        {major.map((pick, i) => {
+                            return (
+                                <div key={i}>
+                                    {modifyInfo.specialty === pick ? (
+                                        <button
+                                            className={styles.specPick}
+                                            onClick={() => {
+                                                setModifyInfo({ ...modifyInfo, specialty: pick });
+                                            }}>
+                                            <Check />
+                                            {pick}
+                                        </button>
+                                    ) : (
+                                        <button
+                                            className={styles.specAnother}
+                                            onClick={() => {
+                                                setModifyInfo({ ...modifyInfo, specialty: pick });
+                                            }}>
+                                            {pick}
+                                        </button>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
 };
 export default ModifyProfile;
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { __getRanking } from '../app/slice/rankingSlice';
-import Footer from '../components/footer/Footer';
-import Layout from '../layout/Layout';
-import styles from './css/rankingPage.module.css';
-import dropdownBtn from '../common/svg/dropdown_icon.svg';
-import check from '../common/svg/check_icon.svg';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { __getRanking } from "../app/slice/rankingSlice";
+import Footer from "../components/footer/Footer";
+import Layout from "../layout/Layout";
+import styles from "./css/rankingPage.module.css";
+import dropdownBtn from "../common/svg/dropdown_icon.svg";
+import check from "../common/svg/check_icon.svg";
 
 const Ranking = () => {
     const dispatch = useDispatch();
@@ -17,19 +17,19 @@ const Ranking = () => {
     const [showSheet, setShowSheet] = useState(false);
     const [btsOn, setBtsOn] = useState(false);
     const agePick = [
-        { ko: '전체 랭킹', en: 'all' },
-        { ko: '20대 랭킹', en: 'twenty' },
-        { ko: '30대 랭킹', en: 'thirty' },
+        { ko: "전체 랭킹", en: "all" },
+        { ko: "20대 랭킹", en: "twenty" },
+        { ko: "30대 랭킹", en: "thirty" },
     ];
     const datePick = [
-        { ko: '일간', en: 'day' },
-        { ko: '주간', en: 'week' },
-        { ko: '월간', en: 'month' },
+        { ko: "일간", en: "day" },
+        { ko: "주간", en: "week" },
+        { ko: "월간", en: "month" },
     ];
-    const [mode, setMode] = useState('일간');
-    const [ageMode, setAgeMode] = useState('전체 랭킹');
+    const [mode, setMode] = useState("일간");
+    const [ageMode, setAgeMode] = useState("전체 랭킹");
 
-    const [type, setType] = useState({ period: 'day', category: 'all' });
+    const [type, setType] = useState({ period: "day", category: "all" });
     console.log(type);
 
     useEffect(() => {
@@ -42,7 +42,7 @@ const Ranking = () => {
 
     return (
         <Layout>
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
                 <div>
                     <div className={btsOn ? styles.blurIn : styles.blurOut} onClick={modalOffHandler}>
                         <div className={styles.rankingType}>
@@ -64,8 +64,8 @@ const Ranking = () => {
                                             <button
                                                 className={styles.button}
                                                 style={{
-                                                    backgroundColor: 'var(--neutral-40)',
-                                                    color: 'var(--neutral-100)',
+                                                    backgroundColor: "var(--neutral-40)",
+                                                    color: "var(--neutral-100)",
                                                 }}
                                                 onClick={() => {
                                                     setMode(day.ko);
@@ -77,8 +77,8 @@ const Ranking = () => {
                                             <button
                                                 className={styles.button}
                                                 style={{
-                                                    backgroundColor: 'var(--neutral-30)',
-                                                    color: 'var(--neutral-70)',
+                                                    backgroundColor: "var(--neutral-30)",
+                                                    color: "var(--neutral-70)",
                                                 }}
                                                 onClick={() => {
                                                     setType({ ...type, period: day.en });
@@ -98,23 +98,22 @@ const Ranking = () => {
                                 const minutes = parseInt((userTimeSet % 3600) / 60);
                                 return (
                                     <div key={i}>
-                                        <div
-                                            className={
-                                                i === 0 ? styles.topRanker : i === 1 || i === 2 ? styles.otherRanker : styles.allStatus
-                                            }>
-                                            <span className={i > 2 ? styles.padding : styles.userRank}>
-                                                {i === 0 && '👑'}
-                                                {i === 1 && '🥈'}
-                                                {i === 2 && '🥉'}
-                                                {i + 1}
-                                            </span>
-                                            <div className={styles.userBox}>
-                                                <p className={styles.userNickname}>{rankbox.nickname}</p>
-                                                <p className={styles.userSpec}> {rankbox.specialty}</p>
+                                        <div className={i === 0 ? styles.topRanker : i === 1 || i === 2 ? styles.otherRanker : styles.allStatus}>
+                                            <div className={styles.nameBox}>
+                                                <span className={i > 2 ? styles.padding : styles.userRank}>
+                                                    {i === 0 && "👑"}
+                                                    {i === 1 && "🥈"}
+                                                    {i === 2 && "🥉"}
+                                                    {i + 1}
+                                                </span>
+                                                <div className={styles.userBox}>
+                                                    <p className={styles.userNickname}>{rankbox.nickname}</p>
+                                                    <p className={styles.userSpec}> {rankbox.specialty}</p>
+                                                </div>
                                             </div>
                                             <div className={styles.timeBox}>
                                                 <span className={styles.userTime}>
-                                                    {hour < 10 ? '0' + hour : hour}시간 {minutes < 10 ? '0' + minutes : minutes}분
+                                                    {hour < 10 ? "0" + hour : hour}시간 {minutes < 10 ? "0" + minutes : minutes}분
                                                 </span>
                                                 {rankbox.studying ? (
                                                     <div className={i === 0 ? styles.rankerDot : styles.greendot}></div>
@@ -129,21 +128,23 @@ const Ranking = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{ position: 'relative' }}>
+                <div style={{ position: "relative" }}>
                     <div className={styles.myStatus}>
-                        <span className={getMyRanking.rank > 2 ? styles.padding : styles.userRank}>
-                            {getMyRanking.rank === 0 && '👑'}
-                            {getMyRanking.rank === 1 && '🥈'}
-                            {getMyRanking.rank === 2 && '🥉'}
-                            {getMyRanking.rank}
-                        </span>
-                        <div className={styles.userBox}>
-                            <p className={styles.userNickname}>{getMyRanking.nickname}</p>
-                            <p className={styles.userSpec}>{getMyRanking.specialty}</p>
+                        <div className={styles.nameBox}>
+                            <span className={getMyRanking.rank > 2 ? styles.padding : styles.userRank}>
+                                {getMyRanking.rank === 0 && "👑"}
+                                {getMyRanking.rank === 1 && "🥈"}
+                                {getMyRanking.rank === 2 && "🥉"}
+                                {getMyRanking.rank}
+                            </span>
+                            <div className={styles.userBox}>
+                                <p className={styles.userNickname}>{getMyRanking.nickname}</p>
+                                <p className={styles.userSpec}>{getMyRanking.specialty}</p>
+                            </div>
                         </div>
                         <div className={styles.timeBox}>
                             <span className={styles.userTime}>
-                                {myHour < 10 ? '0' + myHour : myHour}시간 {myMinutes < 10 ? '0' + myMinutes : myMinutes}분
+                                {myHour < 10 ? "0" + myHour : myHour}시간 {myMinutes < 10 ? "0" + myMinutes : myMinutes}분
                             </span>
                             {getMyRanking.studying ? <div className={styles.greendot}></div> : <div className={styles.emptyDot}></div>}
                         </div>
@@ -158,7 +159,7 @@ const Ranking = () => {
                             {ageMode === age.ko ? (
                                 <button
                                     className={styles.ageChoice}
-                                    style={{ color: '#ffffff' }}
+                                    style={{ color: "#ffffff" }}
                                     onClick={() => {
                                         setAgeMode(age.ko);
                                         setType({ ...type, category: age.en });
@@ -169,7 +170,7 @@ const Ranking = () => {
                             ) : (
                                 <button
                                     className={styles.ageChoice}
-                                    style={{ color: '#7E7C8C' }}
+                                    style={{ color: "#7E7C8C" }}
                                     onClick={() => {
                                         setType({ ...type, category: age.en });
                                         setAgeMode(age.ko);
