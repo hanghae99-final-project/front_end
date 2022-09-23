bf56cf370f54b35eb3bf60f354a9c192a1757d17
H4Nhs <gksgytmd19@kakao.com>
2022-09-23 23:12:34 +0900

    feature : 마이페이지 로그아웃 일부 구현  design : 프로필 수정 반응형 구현 ,버튼 active 동작

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import WeeklyDataGraph from "../components/weeklyGraph/WeeklyDataGraph";
 import Layout from "../layout/Layout";
 import Footer from "../components/footer/Footer";
@@ -8,12 +8,26 @@ import { ReactComponent as Event } from "../common/svg/event_available.svg";
 import styles from "./css/mypage.module.css";
 import ProfileTodoList from "../components/profile/ProfileTodoList";
 import Calender from "../components/calender/Calender";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import font from "../common/css/font.module.css";
 
 const MyPage = () => {
+    console.log(localStorage.getItem("token"));
+    const navi = useNavigate();
+
+    useEffect(() => {
+        localStorage.getItem("token") === null && navi("/");
+    }, []);
+    const logout = () => {
+        localStorage.removeItem("token");
+        navi("/");
+    };
+
     return (
         <Layout>
+            <button className={`${styles.logoutBtn} ${styles.subtitle4_600_12}`} onClick={logout}>
+                로그아웃
+            </button>
             <div className={styles.box}>
                 <MyPageStudyTime />
                 <div className={styles.buttonBox}>
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -8,34 +8,33 @@ import dropdownBtn from "../common/svg/dropdown_icon.svg";
 import check from "../common/svg/check_icon.svg";
 
 const Ranking = () => {
-  const dispatch = useDispatch();
-  const getMyRanking = useSelector((state) => state.ranking.myRanking);
-  const getAllRanking = useSelector((state) => state.ranking.ranking);
-  const userTimeSet = Math.floor(getMyRanking.savedStudyTime / 1000);
-  const myHour = parseInt(userTimeSet / 3600);
-  const myMinutes = parseInt((userTimeSet % 3600) / 60);
-  const [showSheet, setShowSheet] = useState(false);
-  const [btsOn, setBtsOn] = useState(false);
-  const agePick = [
-    { ko: "전체 랭킹", en: "all" },
-    { ko: "20대 랭킹", en: "twenty" },
-    { ko: "30대 랭킹", en: "thirty" },
-  ];
-  const datePick = [
-    { ko: "일간", en: "day" },
-    { ko: "주간", en: "week" },
-    { ko: "월간", en: "month" },
-  ];
-  const [mode, setMode] = useState("일간");
-  const [ageMode, setAgeMode] = useState("전체 랭킹");
+    const dispatch = useDispatch();
+    const getMyRanking = useSelector((state) => state.ranking.myRanking);
+    const getAllRanking = useSelector((state) => state.ranking.ranking);
+    const userTimeSet = Math.floor(getMyRanking.savedStudyTime / 1000);
+    const myHour = parseInt(userTimeSet / 3600);
+    const myMinutes = parseInt((userTimeSet % 3600) / 60);
+    const [showSheet, setShowSheet] = useState(false);
+    const [btsOn, setBtsOn] = useState(false);
+    const agePick = [
+        { ko: "전체 랭킹", en: "all" },
+        { ko: "20대 랭킹", en: "twenty" },
+        { ko: "30대 랭킹", en: "thirty" },
+    ];
+    const datePick = [
+        { ko: "일간", en: "day" },
+        { ko: "주간", en: "week" },
+        { ko: "월간", en: "month" },
+    ];
+    const [mode, setMode] = useState("일간");
+    const [ageMode, setAgeMode] = useState("전체 랭킹");
 
-  const [type, setType] = useState({ period: "day", category: "all" });
-  console.log(type);
+    const [type, setType] = useState({ period: "day", category: "all" });
 
-  useEffect(() => {
-    dispatch(__getRanking(type));
-  }, [type]);
-  
+    useEffect(() => {
+        dispatch(__getRanking(type));
+    }, [type]);
+
     const modalOffHandler = (e) => {
         setBtsOn(false);
     };
@@ -47,19 +46,21 @@ const Ranking = () => {
                     flexDirection: "column",
                     justifyContent: "space-between",
                     height: "100%",
-                }}>
+                }}
+            >
                 <div>
                     <div className={btsOn ? styles.blurIn : styles.blurOut} onClick={modalOffHandler}>
                         <div className={styles.rankingType}>
-                            {ageMode}
-                            <button
-                                className={styles.dropdownBtn}
+                            <div
+                                className={styles.topBtn}
                                 onClick={(e) => {
                                     e.stopPropagation();
                                     setBtsOn(!btsOn);
-                                }}>
-                                <img src={dropdownBtn} alt='dropdownBtn' />
-                            </button>
+                                }}
+                            >
+                                <p>{ageMode}</p>
+                                <div className={styles.dropdownBtn} src={dropdownBtn} alt="arrowBtn2"></div>
+                            </div>
                         </div>
                         <div className={styles.dateRanking}>
                             {datePick.map((day, i) => {
@@ -75,7 +76,8 @@ const Ranking = () => {
                                                 onClick={() => {
                                                     setMode(day.ko);
                                                     setType({ ...type, period: day.en });
-                                                }}>
+                                                }}
+                                            >
                                                 {day.ko}
                                             </button>
                                         ) : (
@@ -88,7 +90,8 @@ const Ranking = () => {
                                                 onClick={() => {
                                                     setType({ ...type, period: day.en });
                                                     setMode(day.ko);
-                                                }}>
+                                                }}
+                                            >
                                                 {day.ko}
                                             </button>
                                         )}
@@ -153,78 +156,44 @@ const Ranking = () => {
                             </span>
                             {getMyRanking.studying ? <div className={styles.greendot}></div> : <div className={styles.emptyDot}></div>}
                         </div>
-                      </div>
-                      <div className={styles.timeBox}>
-                        <span className={styles.userTime}>
-                          {hour < 10 ? "0" + hour : hour}시간 {minutes < 10 ? "0" + minutes : minutes}분
-                        </span>
-                        {rankbox.studying ? <div className={i === 0 ? styles.rankerDot : styles.greendot}></div> : <div className={styles.emptyDot}></div>}
-                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                    <Footer />
+                </div>
             </div>
-          </div>
-        </div>
-        <div style={{ position: "relative" }}>
-          <div className={styles.myStatus}>
-            <div className={styles.nameBox}>
-              <span className={getMyRanking.rank > 2 ? styles.padding : styles.userRank}>
-                {getMyRanking.rank === 1 && "👑"}
-                {getMyRanking.rank === 2 && "🥈"}
-                {getMyRanking.rank === 3 && "🥉"}
-                {getMyRanking.rank ? getMyRanking.rank : "--"}
-              </span>
-              <div className={styles.userBox}>
-                <p className={styles.userNickname}>{getMyRanking.nickname}</p>
-                <p className={styles.userSpec}>{getMyRanking.specialty}</p>
-              </div>
+            <div className={btsOn ? styles.btsOn : styles.btsOff}>
+                {agePick.map((age, i) => {
+                    return (
+                        <div key={i} className={styles.btsWrap}>
+                            {ageMode === age.ko ? (
+                                <button
+                                    className={styles.ageChoice}
+                                    style={{ color: "#ffffff" }}
+                                    onClick={() => {
+                                        setAgeMode(age.ko);
+                                        setType({ ...type, category: age.en });
+                                    }}
+                                >
+                                    {age.ko}
+                                    <img src={check} alt="check" />
+                                </button>
+                            ) : (
+                                <button
+                                    className={styles.ageChoice}
+                                    style={{ color: "#7E7C8C" }}
+                                    onClick={() => {
+                                        setType({ ...type, category: age.en });
+                                        setAgeMode(age.ko);
+                                    }}
+                                >
+                                    {age.ko}
+                                </button>
+                            )}
+                        </div>
+                    );
+                })}
             </div>
-            <div className={styles.timeBox}>
-              <span className={styles.userTime}>
-                {myHour < 10 ? "0" + myHour : myHour}시간 {myMinutes < 10 ? "0" + myMinutes : myMinutes}분
-              </span>
-              {getMyRanking.studying ? <div className={styles.greendot}></div> : <div className={styles.emptyDot}></div>}
-            </div>
-          </div>
-          <Footer />
-        </div>
-      </div>
-      <div className={btsOn ? styles.btsOn : styles.btsOff}>
-        {agePick.map((age, i) => {
-          return (
-            <div key={i} className={styles.btsWrap}>
-              {ageMode === age.ko ? (
-                <button
-                  className={styles.ageChoice}
-                  style={{ color: "#ffffff" }}
-                  onClick={() => {
-                    setAgeMode(age.ko);
-                    setType({ ...type, category: age.en });
-                  }}
-                >
-                  {age.ko}
-                  <img src={check} alt="check" />
-                </button>
-              ) : (
-                <button
-                  className={styles.ageChoice}
-                  style={{ color: "#7E7C8C" }}
-                  onClick={() => {
-                    setType({ ...type, category: age.en });
-                    setAgeMode(age.ko);
-                  }}
-                >
-                  {age.ko}
-                </button>
-              )}
-            </div>
-          );
-        })}
-      </div>
-    </Layout>
-  );
+        </Layout>
+    );
 };
 
 export default Ranking;
