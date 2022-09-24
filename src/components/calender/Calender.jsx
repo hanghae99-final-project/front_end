3618735f2d67a1b91d00e138095ac72d44d4eda2
SuHwan,Kim (김수환) <shkiimaa@gmail.com>
2022-09-25 03:50:23 +0900

    fix: mypage todolist 버그 수정

--- src/components/calender/Calender.jsx
+++ src/components/calender/Calender.jsx
@@ -5,199 +5,185 @@ import styles from "./calender.module.css";
 import font from "../../common/css/common.module.css";
 
 const Calender = () => {
-    const dispatch = useDispatch();
-    const studyData = useSelector((state) => state.calender);
+  const dispatch = useDispatch();
+  const studyData = useSelector(state => state.calender);
 
-    const today = {
-        year: new Date().getFullYear(), //오늘 연도
-        month: new Date().getMonth() + 1, //오늘 월
-        date: new Date().getDate(), //오늘 날짜
-        day: new Date().getDay(), //오늘 요일
-    };
+  const today = {
+    year: new Date().getFullYear(), //오늘 연도
+    month: new Date().getMonth() + 1, //오늘 월
+    date: new Date().getDate(), //오늘 날짜
+    day: new Date().getDay() //오늘 요일
+  };
 
-    console.log(today.month, today.date);
+  console.log(today.month, today.date);
 
-    const [selectedYear, setSelectedYear] = useState(today.year); //현재 선택된 연도
-    const [selectedMonth, setSelectedMonth] = useState(today.month); //현재 선택된 달
-    const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate(); //선택된 연도, 달의 마지막 날짜
+  const [selectedYear, setSelectedYear] = useState(today.year); //현재 선택된 연도
+  const [selectedMonth, setSelectedMonth] = useState(today.month); //현재 선택된 달
+  const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate(); //선택된 연도, 달의 마지막 날짜
 
-    useEffect(() => {
-        dispatch(getMonthList({ selectedYear, selectedMonth }));
-    }, [selectedMonth]);
+  useEffect(() => {
+    dispatch(getMonthList({ selectedYear, selectedMonth }));
+  }, [selectedMonth]);
 
-    //달력 월의 시작 요일
-    const startMonth = new Date(selectedYear, selectedMonth - 1, 1);
-    const calendarStartMonthData = startMonth.getDay();
+  //달력 월의 시작 요일
+  const startMonth = new Date(selectedYear, selectedMonth - 1, 1);
+  const calendarStartMonthData = startMonth.getDay();
 
-    //한달 주 수
-    const weekCount = Math.ceil((dateTotalCount + calendarStartMonthData) / 7);
+  //한달 주 수
+  const weekCount = Math.ceil((dateTotalCount + calendarStartMonthData) / 7);
 
-    //일주일
-    const week = ["일", "월", "화", "수", "목", "금", "토"];
+  //일주일
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
 
-    //이전 달 보기 버튼
-    const prevMonth = useCallback(() => {
-        if (selectedMonth === 1) {
-            setSelectedMonth(12);
-            setSelectedYear(selectedYear - 1);
-        } else {
-            setSelectedMonth(selectedMonth - 1);
-        }
-    }, [selectedMonth]);
-
-    //다음 달 보기 버튼
-    const nextMonth = useCallback(() => {
-        if (selectedMonth === 12) {
-            setSelectedMonth(1);
-            setSelectedYear(selectedYear + 1);
-        } else {
-            setSelectedMonth(selectedMonth + 1);
-        }
-    }, [selectedMonth]);
-
-    // 선택한 월 일 갯수
-    const selectMonthData = [];
-    for (let i = 0; i <= dateTotalCount; i++) {
-        selectMonthData.push(i);
+  //이전 달 보기 버튼
+  const prevMonth = useCallback(() => {
+    if (selectedMonth === 1) {
+      setSelectedMonth(12);
+      setSelectedYear(selectedYear - 1);
+    } else {
+      setSelectedMonth(selectedMonth - 1);
     }
-    console.log(selectMonthData);
+  }, [selectedMonth]);
 
-    // 선택한 월에 맞는 요일 및 날짜 바인딩
+  //다음 달 보기 버튼
+  const nextMonth = useCallback(() => {
+    if (selectedMonth === 12) {
+      setSelectedMonth(1);
+      setSelectedYear(selectedYear + 1);
+    } else {
+      setSelectedMonth(selectedMonth + 1);
+    }
+  }, [selectedMonth]);
 
-    function calenderDate() {
-        const data = [];
-        let calenderPos = 0;
-        let calenderDay = 0;
-        let todayValue = new Date(today.year, today.month - 1, today.date + 1).toISOString().split("T")[0];
+  // 선택한 월 일 갯수
+  const selectMonthData = [];
+  for (let i = 0; i <= dateTotalCount; i++) {
+    selectMonthData.push(i);
+  }
 
-        for (let i = 0; i < weekCount; i++) {
-            for (let i2 = 0; i2 < 7; i2++) {
-                let dayValue = new Date(selectedYear, selectedMonth - 1, calenderDay + 2).toISOString().split("T")[0];
+  // 선택한 월에 맞는 요일 및 날짜 바인딩
 
-                let ifValue =
-                    studyData.map((data) => data.studyDate).includes(dayValue) && studyData.filter((data) => data.studyDate === dayValue)[0].studyTime;
-                let hours = 3600000;
+  function calenderDate() {
+    const data = [];
+    let calenderPos = 0;
+    let calenderDay = 0;
+    let todayValue = new Date(today.year, today.month - 1, today.date + 1).toISOString().split("T")[0];
 
-                /* if (todayValue === dayValue) {
+    for (let i = 0; i < weekCount; i++) {
+      for (let i2 = 0; i2 < 7; i2++) {
+        let dayValue = new Date(selectedYear, selectedMonth - 1, calenderDay + 2).toISOString().split("T")[0];
+
+        let ifValue =
+          studyData.map(data => data.studyDate).includes(dayValue) &&
+          studyData.filter(data => data.studyDate === dayValue)[0].studyTime;
+        let hours = 3600000;
+
+        if (studyData.map(data => data.studyDate).includes(dayValue) === false) {
           data.push(
             <div
-              className={styles.dateWarp}
-              style={{ backgroundColor: "0 0 0 3px, red, inset;" }}
+              className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
+              style={{ backgroundColor: "#4b4a56" }}
             >
-              {calendarStartMonthData <= calenderPos &&
-              calenderDay < dateTotalCount
+              {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
                 ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
                 : ""}
             </div>
           );
-        } else  */
-                if (studyData.map((data) => data.studyDate).includes(dayValue) === false) {
-                    data.push(
-                        <div
-                            className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
-                            style={{ backgroundColor: "#4b4a56" }}
-                        >
-                            {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
-                                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
-                                : ""}
-                        </div>
-                    );
-                } else if (ifValue < hours * 3) {
-                    data.push(
-                        <div
-                            className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
-                            style={{ backgroundColor: "#3a4940" }}
-                        >
-                            {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
-                                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
-                                : ""}
-                        </div>
-                    );
-                } else if (ifValue >= hours * 3 && ifValue < hours * 6) {
-                    data.push(
-                        <div
-                            className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
-                            style={{ backgroundColor: "#447257" }}
-                        >
-                            {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
-                                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
-                                : ""}
-                        </div>
-                    );
-                } else if (ifValue >= hours * 6 && ifValue < hours * 9) {
-                    data.push(
-                        <div
-                            className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
-                            style={{ backgroundColor: "#4e9a6e" }}
-                        >
-                            {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
-                                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
-                                : ""}
-                        </div>
-                    );
-                } else if (ifValue >= hours * 9 && ifValue < hours * 12) {
-                    data.push(
-                        <div
-                            className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
-                            style={{ backgroundColor: "#66ffa6" }}
-                        >
-                            {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
-                                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
-                                : ""}
-                        </div>
-                    );
-                } else {
-                    data.push(
-                        <div
-                            className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
-                            style={{ backgroundColor: "#ff8058" }}
-                        >
-                            {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
-                                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
-                                : ""}
-                        </div>
-                    );
-                }
-                calenderPos++;
-            }
-        }
-        return data;
-    }
-
-    function calenderData() {
-        const data = [];
-        data.push(<div className={styles.dateContainer}> {calenderDate()} </div>);
-
-        return data;
-    }
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.innerWarp}>
-                <div className={styles.dateBtnWarp}>
-                    <button className={styles.leftBtn} onClick={prevMonth}></button>
-                    <h2 className={font.caption_600_12}>
-                        {selectedYear}년 {selectedMonth}월
-                    </h2>
-                    <button className={styles.rightBtn} onClick={nextMonth}></button>
-                </div>
-
-                <div className={styles.weekWrap}>
-                    <div className={styles.dayWarp}>
-                        {week.map((data, idx) => {
-                            console.log(data);
-                            return (
-                                <div className={`${styles.day} ${font.caption3_600_8}`} key={idx}>
-                                    <span>{data}</span>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    {calenderData()}
-                </div>
+        } else if (ifValue < hours * 3) {
+          data.push(
+            <div
+              className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
+              style={{ backgroundColor: "#3a4940" }}
+            >
+              {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
+                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
+                : ""}
             </div>
+          );
+        } else if (ifValue >= hours * 3 && ifValue < hours * 6) {
+          data.push(
+            <div
+              className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
+              style={{ backgroundColor: "#447257" }}
+            >
+              {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
+                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
+                : ""}
+            </div>
+          );
+        } else if (ifValue >= hours * 6 && ifValue < hours * 9) {
+          data.push(
+            <div
+              className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
+              style={{ backgroundColor: "#4e9a6e" }}
+            >
+              {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
+                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
+                : ""}
+            </div>
+          );
+        } else if (ifValue >= hours * 9 && ifValue < hours * 12) {
+          data.push(
+            <div
+              className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
+              style={{ backgroundColor: "#66ffa6" }}
+            >
+              {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
+                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
+                : ""}
+            </div>
+          );
+        } else {
+          data.push(
+            <div
+              className={todayValue === dayValue ? `${styles.dateWarp} ${styles.selectedToday}` : styles.dateWarp}
+              style={{ backgroundColor: "#ff8058" }}
+            >
+              {calendarStartMonthData <= calenderPos && calenderDay < dateTotalCount
+                ? (calenderDay++, (<span value={dayValue}>{calenderDay}</span>))
+                : ""}
+            </div>
+          );
+        }
+        calenderPos++;
+      }
+    }
+    return data;
+  }
+
+  function calenderData() {
+    const data = [];
+    data.push(<div className={styles.dateContainer}> {calenderDate()} </div>);
+
+    return data;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerWarp}>
+        <div className={styles.dateBtnWarp}>
+          <button className={styles.leftBtn} onClick={prevMonth}></button>
+          <h2 className={font.caption_600_12}>
+            {selectedYear}년 {selectedMonth}월
+          </h2>
+          <button className={styles.rightBtn} onClick={nextMonth}></button>
         </div>
-    );
+
+        <div className={styles.weekWrap}>
+          <div className={styles.dayWarp}>
+            {week.map((data, idx) => {
+              return (
+                <div className={`${styles.day} ${font.caption3_600_8}`} key={idx}>
+                  <span>{data}</span>
+                </div>
+              );
+            })}
+          </div>
+          {calenderData()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Calender;
--- src/components/todo/ToDoList.jsx
+++ src/components/todo/ToDoList.jsx
@@ -7,109 +7,120 @@ import styles from "./toDoList.module.css";
 import font from "../././../common/css/font.module.css";
 
 const ToDoList = () => {
-    const [upDateValue, setUpDateValue] = useState("");
-    const [upDateColor, setUpDateColor] = useState("#FFFFFF");
-    const [btnOn, setBtnOn] = useState(true);
-    const [modifyOn, setModifyOn] = useState("");
-    const [modifyModal, setModifyModal] = useState("");
+  const [upDateValue, setUpDateValue] = useState("");
+  const [upDateColor, setUpDateColor] = useState("#FFFFFF");
+  const [btnOn, setBtnOn] = useState(true);
+  const [modifyOn, setModifyOn] = useState("");
+  const [modifyModal, setModifyModal] = useState("");
 
-    const dispatch = useDispatch();
-    const toDos = useSelector((state) => state.toDo);
-    console.log(toDos);
+  const dispatch = useDispatch();
+  const toDos = useSelector(state => state.toDo);
+  console.log(toDos);
 
-    useEffect(() => {
-        dispatch(getList());
-    }, []);
+  useEffect(() => {
+    dispatch(getList());
+  }, []);
 
-    const onDeleteToDoHandler = (toDoId) => {
-        dispatch(deleteList(toDoId));
-    };
+  const onDeleteToDoHandler = toDoId => {
+    dispatch(deleteList(toDoId));
+  };
 
-    const onClickModifyOnOff = (e) => {
-        console.dir(e.target);
-    };
+  const onClickModifyOnOff = e => {
+    console.dir(e.target);
+  };
 
-    const onChnageTodoValueUpDateHandler = (e) => {
-        setUpDateValue(e.target.value);
-    };
+  const onChnageTodoValueUpDateHandler = e => {
+    setUpDateValue(e.target.value);
+  };
 
-    // const onUpdateToDoHandler = (toDoId) => {
-    //   if (upDateValue !== '') {
-    //     dispatch(updateList({ id: toDoId, work: upDateValue, color: upDateColor }));
-    //   } else {
-    //     alert('수정할 내용을 입력해 주세요.');
-    //   }
-    //   setUpDateValue('');
-    // };
+  // const onUpdateToDoHandler = (toDoId) => {
+  //   if (upDateValue !== '') {
+  //     dispatch(updateList({ id: toDoId, work: upDateValue, color: upDateColor }));
+  //   } else {
+  //     alert('수정할 내용을 입력해 주세요.');
+  //   }
+  //   setUpDateValue('');
+  // };
 
-    const onClickToDoDone = (isDone, toDoId) => {
-        dispatch(updateToDoDone({ isDone: !isDone, id: toDoId }));
-    };
+  const onClickToDoDone = (isDone, toDoId) => {
+    dispatch(updateToDoDone({ isDone: !isDone, id: toDoId }));
+  };
 
-    return (
-        <ul className={styles.container}>
-            {toDos.length === 0 ? (
-                <div className={`${styles.emptyTodo} ${font.subtitle2_600_16}`}>할 일을 추가해 보세요</div>
-            ) : (
-                <div className={styles.innerContainer}>
-                    {toDos
-                        ?.map((toDo) => {
-                            return (
-                                <div className={modifyOn === toDo._id ? `${styles.outWarp} ${styles.modifyOn}` : `${styles.outWarp}`} key={toDo._id}>
-                                    <li className={styles.innerWarp}>
-                                        <div className={styles.toDoContainer}>
-                                            <div className={styles.toDoWarp}>
-                                                <div
-                                                    onClick={() => {
-                                                        modifyOn === toDo._id ? setModifyOn("") : setModifyOn(toDo._id);
-                                                        setModifyModal("");
-                                                    }}
-                                                    className={styles.toDoValueWarp}
-                                                >
-                                                    <PickColor bgColor={toDo.color}></PickColor>
-                                                    <p
-                                                        className={
-                                                            toDo.isDone
-                                                                ? `${styles.ToDoTrue} ${font.strikethrough_30014}`
-                                                                : `${styles.ToDoFalse} ${font.body2_300_14}`
-                                                        }
-                                                    >
-                                                        {toDo.work}
-                                                    </p>
-                                                </div>
-                                                <button
-                                                    className={toDo.isDone ? styles.isDoneTureBtn : styles.isDoneFalseBtn}
-                                                    onClick={() => onClickToDoDone(toDo.isDone, toDo._id)}
-                                                ></button>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <div className={styles.btnWarp}>
-                                        <button
-                                            className={styles.modifyBtn}
-                                            onClick={() => {
-                                                modifyModal === toDo._id ? setModifyModal("") : setModifyModal(toDo._id);
-                                            }}
-                                        ></button>
-                                        <button className={styles.delBtn} onClick={() => onDeleteToDoHandler(toDo._id)} type="button"></button>
-                                    </div>
-                                    {modifyModal === toDo._id ? <ToDoModify toDoId={toDo._id} setModifyModal={setModifyModal} setModifyOn={setModifyOn} /> : ""}
-                                </div>
-                            );
-                        })
-                        .reverse()}
+  return (
+    <ul className={styles.container}>
+      {toDos.length === 0 ? (
+        <div className={`${styles.emptyTodo} ${font.subtitle2_600_16}`}>할 일을 추가해 보세요</div>
+      ) : (
+        <div className={styles.innerContainer}>
+          {toDos
+            ?.map(toDo => {
+              return (
+                <div
+                  className={modifyOn === toDo._id ? `${styles.outWarp} ${styles.modifyOn}` : `${styles.outWarp}`}
+                  key={toDo._id}
+                >
+                  <li className={styles.innerWarp}>
+                    <div className={styles.toDoContainer}>
+                      <div className={styles.toDoWarp}>
+                        <div
+                          onClick={() => {
+                            modifyOn === toDo._id ? setModifyOn("") : setModifyOn(toDo._id);
+                            setModifyModal("");
+                          }}
+                          className={styles.toDoValueWarp}
+                        >
+                          <PickColor bgColor={toDo.color}></PickColor>
+                          <p
+                            className={
+                              toDo.isDone
+                                ? `${styles.ToDoTrue} ${font.strikethrough_30014}`
+                                : `${styles.ToDoFalse} ${font.body2_300_14}`
+                            }
+                          >
+                            {toDo.work}
+                          </p>
+                        </div>
+                        <button
+                          className={toDo.isDone ? styles.isDoneTureBtn : styles.isDoneFalseBtn}
+                          onClick={() => onClickToDoDone(toDo.isDone, toDo._id)}
+                        ></button>
+                      </div>
+                    </div>
+                  </li>
+                  <div className={styles.btnWarp}>
+                    <button
+                      className={styles.modifyBtn}
+                      onClick={() => {
+                        modifyModal === toDo._id ? setModifyModal("") : setModifyModal(toDo._id);
+                      }}
+                    ></button>
+                    <button
+                      className={styles.delBtn}
+                      onClick={() => onDeleteToDoHandler(toDo._id)}
+                      type="button"
+                    ></button>
+                  </div>
+                  {modifyModal === toDo._id ? (
+                    <ToDoModify toDoId={toDo._id} setModifyModal={setModifyModal} setModifyOn={setModifyOn} />
+                  ) : (
+                    ""
+                  )}
                 </div>
-            )}
-        </ul>
-    );
+              );
+            })
+            .reverse()}
+        </div>
+      )}
+    </ul>
+  );
 };
 
 const PickColor = styled.div`
-    width: 0.25rem;
-    height: 0.25rem;
-    border-radius: 50%;
-    margin: 0.5rem 0.5rem 0.5rem 0;
-    background-color: ${(props) => props.bgColor};
+  width: 0.25rem;
+  height: 0.25rem;
+  border-radius: 50%;
+  margin: 0.5rem 0.5rem 0.5rem 0;
+  background-color: ${props => props.bgColor};
 `;
 
 export default ToDoList;
