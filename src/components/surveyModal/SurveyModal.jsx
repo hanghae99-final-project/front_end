429325d2271170452504ff2e20ecd4182479b953
Jongco <narira0922@gmail.com>
2022-09-25 22:50:14 +0900

    chore: 폴더 오타 수정 및 설문조사 페이지 연결

---
+++ src/components/surveyModal/SurveyModal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./surveyModal.module.css";
+import font from "../../common/css/font.module.css";
+import survey from "../../common/svg/surveyImg.png";
+
+const SurveyModal = ({ setSurvey }) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.imgBox}>
+        <img src={survey} alt="설문조사" className={styles.surveyImg} />
+        <div className={`${styles.surveyText} ${font.subtitle2_300_16}`}>
+          <div>
+            랭플 회원들에게 <span className={styles.accent}>치킨쏜닭</span> !
+          </div>
+          <div>지금 설문조사 참여하고 치킨받자🍗</div>
+        </div>
+      </div>
+      <button
+        className={`${styles.chicken} ${font.subtitle2_600_16}`}
+        onClick={() => {
+          // if (window.confirm("설문조사에 참여하시겠습니까?")) {
+          window.location.replace("https://forms.gle/nPxMeV8PRDZzahTd9");
+          // }
+        }}
+      >
+        치킨 받으러 가기
+      </button>
+      <button
+        className={`${styles.close} ${font.subtitle2_600_16}`}
+        onClick={() => {
+          setSurvey(false);
+        }}
+      >
+        닫기
+      </button>
+    </div>
+  );
+};
+
+export default SurveyModal;
