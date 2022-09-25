25406f0011d063d9a67a90072457c7bf63d1c265
SuHwan,Kim (김수환) <shkiimaa@gmail.com>
2022-09-26 01:52:35 +0900

    design: mypage D-day css수정

--- src/components/todo/TodoBtn.jsx
+++ src/components/todo/TodoBtn.jsx
@@ -5,27 +5,27 @@ import styles from "./todoBtn.module.css";
 import font from "../../common/css/common.module.css";
 
 const TodoBtn = () => {
-    const dispatch = useDispatch();
-    const toDos = useSelector((state) => state.toDo);
+  const dispatch = useDispatch();
+  const toDos = useSelector(state => state.toDo);
 
-    const totalTodo = toDos.length;
-    const isDoneCount = toDos?.filter((data) => data?.isDone === true).length;
+  const totalTodo = toDos.length;
+  const isDoneCount = toDos?.filter(data => data.isDone === true).length;
 
-    useEffect(() => {
-        dispatch(getList());
-    }, []);
+  useEffect(() => {
+    dispatch(getList());
+  }, []);
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.textContainer}>
-                <h2 className={`${font.subtitle2_600_16} ${styles.title}`}>오늘할일</h2>
-                <div className={styles.textWarp}>
-                    <p className={font.subtitle2_600_16}>{isDoneCount}</p>
-                    <p className={`${font.subtitle2_600_16} ${styles.totalTodo}`}>/{totalTodo}</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={`${font.subtitle2_600_16} ${styles.title}`}>오늘할일</h2>
+        <div className={styles.textWarp}>
+          <p className={font.subtitle2_600_16}>{isDoneCount}</p>
+          <p className={`${font.subtitle2_600_16} ${styles.totalTodo}`}>/{totalTodo}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TodoBtn;
