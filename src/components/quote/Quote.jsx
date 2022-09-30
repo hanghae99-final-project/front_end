3b108067ad3cb7918009604c5898db63efb33cd0
Jongco <narira0922@gmail.com>
2022-09-30 11:26:09 +0900

    chore: 불필요한 렌더링 방지 memo 추가

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { get_quote } from '../../app/slice/mainSlice';
-import styles from './quote.module.css';
-import font from '../../common/css/font.module.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { get_quote } from "../../app/slice/mainSlice";
+import styles from "./quote.module.css";
+import font from "../../common/css/font.module.css";
+import { memo } from "react";
 
 const Quote = () => {
-    const dispatch = useDispatch();
-    const quote = useSelector((state) => state.main.quote);
-    useEffect(() => {
-        dispatch(get_quote());
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  const quote = useSelector(state => state.main.quote);
+  useEffect(() => {
+    dispatch(get_quote());
+  }, []);
 
-    return (
-        <div className={styles.heightBox}>
-            <div className={styles.quoteBox}>
-                <span className={`${styles.quotes} ${font.subtitle_600_18}`}>"</span>
-                <div className={`${styles.quote} ${font.body2_300_14}`}>{quote}</div>
-                <span className={`${styles.quotes} ${font.subtitle_600_18}`}>"</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.heightBox}>
+      <div className={styles.quoteBox}>
+        <span className={`${styles.quotes} ${font.subtitle_600_18}`}>"</span>
+        <div className={`${styles.quote} ${font.body2_300_14}`}>{quote}</div>
+        <span className={`${styles.quotes} ${font.subtitle_600_18}`}>"</span>
+      </div>
+    </div>
+  );
 };
 
-export default Quote;
+export default memo(Quote);
